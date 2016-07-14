593b554259eedf074a0f8ea16ca9195e30b6f73a
Dylanl19 <dylanl19109051@tas.tw>
2016-07-14 10:23:03 +0800

    Added UI to drink menu activity

--- SimpleUI/app/src/main/java/com/example/user/simpleui/DrinkMenuActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/DrinkMenuActivity.java
@@ -2,12 +2,22 @@ package com.example.user.simpleui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class DrinkMenuActivity extends AppCompatActivity {
+
+    TextView totalTextView;
+    ListView drinkMenuListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_menu);
+        Log.d("Debug", "DrinkMenuActivity OnCreate");
+
+        totalTextView = (TextView)findViewById(R.id.totalTextView);
+        drinkMenuListView = (ListView)findViewById(R.id.drinkMenuListView);
     }
 }
