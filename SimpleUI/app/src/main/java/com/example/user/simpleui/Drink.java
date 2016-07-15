8472569ec0a96fd89bee876b1b941c76188694e6
Dylanl19 <dylanl19109051@tas.tw>
2016-07-15 12:32:03 +0800

    Added Fragment and Classes

--- SimpleUI/app/src/main/java/com/example/user/simpleui/Drink.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/Drink.java
@@ -1,21 +1,44 @@
 package com.example.user.simpleui;
 
 import org.json.JSONObject;
+import org.json.JSONException;
+
+/**
+ * Created by user on 2016/7/14.
+ */
 
 /**
  * Created by user on 2016/7/14.
  */
 public class Drink {
     String name;
-    int mPrice;
-    int lPrice;
+    int mPrice = 0;
+    int lPrice = 0;
     int imageId;
 
-    public JSONObject{
+    public JSONObject getJsonObject()
+    {
         JSONObject jsonObject = new JSONObject();
-        try{
-            jsonObject.put("");
+        try {
+            jsonObject.put("name", name);
+            jsonObject.put("price", mPrice);
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
+        return jsonObject;
     }
+    public static Drink newInstanceWithData(String Data)
+    {
+        Drink drink = new Drink();
+        try {
+            JSONObject jsonObject = new JSONObject(Data);
+            drink.name = jsonObject.getString("name");
+            drink.lPrice = jsonObject.getInt("lPrice");
+            drink.mPrice = jsonObject.getInt("mPrice");
 
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return drink;
+    }
 }
--- SimpleUI/app/src/main/java/com/example/user/simpleui/DrinkMenuActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/DrinkMenuActivity.java
@@ -1,5 +1,10 @@
 package com.example.user.simpleui;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,8 +15,11 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-public class DrinkMenuActivity extends AppCompatActivity {
+
+public class DrinkMenuActivity extends AppCompatActivity implements DrinkOrderDialog.OnDrinkOrderListener {
 
     TextView totalTextView;
     ListView drinkMenuListView;
@@ -22,7 +30,7 @@ public class DrinkMenuActivity extends AppCompatActivity {
 
 
     List<Drink> drinks = new ArrayList<>();
-    List<Drink> orders = new ArrayList<>();
+    List<DrinkOrder> orders = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,34 +42,9 @@ public class DrinkMenuActivity extends AppCompatActivity {
         drinkMenuListView = (ListView)findViewById(R.id.drinkMenuListView);
         setupDrinkMenuListView();
     }
-
-    private void setupDrinkMenuListView()
-    {
-        DrinkAdapter adapter = new DrinkAdapter(this, drinks);
-        drinkMenuListView.setAdapter(adapter);
-
-        drinkMenuListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                       @Override
-                       public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                               DrinkAdapter drinkAdapter = (DrinkAdapter)parent.getAdapter();
-                               Drink drink = (Drink)drinkAdapter.getItem(position);
-                                orders.add(drink);
-                              updateTotal();
-                           }
-                  });
-    }
-    public void updateTotal()
-    {
-              int total = 0;
-               for(Drink drink: orders)
-               {
-                           total += drink.mPrice;
-               }
-                      totalTextView.setText(String.valueOf(total));
-    }
     private void setData()
     {
-        for(int i = 0; i < names.length; i ++)
+        for(int i = 0; i < names.length; i++)
         {
             Drink drink = new Drink();
             drink.name = names[i];
@@ -70,5 +53,126 @@ public class DrinkMenuActivity extends AppCompatActivity {
             drink.imageId = imageId[i];
             drinks.add(drink);
         }
+    }
+
+
+    private void setupDrinkMenuListView()
+    {
+        DrinkAdapter adapter = new DrinkAdapter(this, drinks);
+        drinkMenuListView.setAdapter(adapter);
+
+        drinkMenuListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                DrinkAdapter drinkAdapter = (DrinkAdapter)parent.getAdapter();
+                Drink drink = (Drink)drinkAdapter.getItem(position);
+                showDrinkOrderDialog(drink);
+            }
+        });
+    }
+
+
+    public void showDrinkOrderDialog(Drink drink)
+    {
+        DrinkOrder drinkOrder = new DrinkOrder(drink);
+
+        FragmentManager fragmentManager = getFragmentManager();
+
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+
+        DrinkOrderDialog dialog = DrinkOrderDialog.newInstance(drinkOrder);
+        Fragment prev = getFragmentManager().findFragmentByTag("DrinkOrderDialog");
+        if(prev != null)
+        {
+            ft.remove(prev);
+        }
+        //ft.replace(R.id.root, dialog);
+        ft.addToBackStack(null);
+       // ft.commit();
+        dialog.show(ft, "DrinkOrderDialog");
+    }
+    public void updateTotal()
+    {
+        int total = 0;
+        for(DrinkOrder order: orders)
+        {
+            total += order.mNumber * order.drink.mPrice + order.lNumber * order.drink.lPrice;
+        }
+
+        totalTextView.setText(String.valueOf(total));
+    }
+
+    public void done(View view)
+    {
+        Intent intent = new Intent();
+
+        JSONArray jsonArray = new JSONArray();
+
+        for(DrinkOrder order : orders)
+        {
+           String data = order.toData();
+            jsonArray.put(data);
+        }
+
+        intent.putExtra("results", jsonArray.toString());
+
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Debug", "DrinkMenuActivity OnStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Debug", "DrinkMenuActivity OnResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Debug", "DrinkMenuActivity OnPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Debug", "DrinkMenuActivity OnStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Debug", "DrinkMenuActivity OnDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("Debug", "DrinkMenuActivity OnRestart");
+    }
+
+    @Override
+    public void onDrinkOrderFinished(DrinkOrder drinkOrder) {
+        Boolean flag = false;
+        for(int i = 0; i < orders.size(); i ++)
+       {
+           if(orders.get(i).drink.name.equals(drinkOrder.drink.name) )
+           {
+               orders.set(i, drinkOrder);
+                flag = true;
+               break;
+           }
+           if(!flag) {
+           orders.add(drinkOrder);
+           }
+           updateTotal();
+       }
+        orders.add(drinkOrder);
+
     }
 }
